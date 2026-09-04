f042d2e27b9562055028295bc42160d9b882ccdd
lol shark <siddhaarthsharma360@gmail.com>
2026-09-04 16:36:03 +0530

    Changes made!!

--- circuits/assign_keyword_tb.v
+++ circuits/assign_keyword_tb.v
@@ -1,15 +1,23 @@
 module assign_use_tb(
     reg Y , X,
-    wire Z
+    wire Z;
 );
 endmodule
 
 assign_use a1(
-    .Y(a),
-    .X(b),
-    .Z(c)
+    .a(Y),
+    .b(X),
+    .c(Z)
 );
 
 initial begin
-    
+    Y = 0 ; X = 0;
+    #10Y = 0 ; X = 1;
+    #10Y = 1 ; X = 0;
+    #10Y = 1 ; X = 1;
 end
+
+initial begin
+    $dumpfile("Waveforms.vcd");
+    $dumpvars(0 , assign_use_tb);
+end    
